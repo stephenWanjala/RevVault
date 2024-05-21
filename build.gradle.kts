d185c1b1c0fbbbb7a3f83bd45fbe1e016247fdac
Wanjala Stephen <stephenwanjala145@gmail.com>
2024-05-22 01:19:52 +0300

    -org.jetbrains.kotlin.plugin.serialization to v2 - io.ktor.plugin to v2.3.11 - Kotlin jvm to v2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ val commonsCodecVersion: String by project
 val kmongoVersion: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.23"
-    id("io.ktor.plugin") version "2.3.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    kotlin("jvm") version "2.0.0"
+    id("io.ktor.plugin") version "2.3.11"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 group = "com.github.stephenwanjala"
