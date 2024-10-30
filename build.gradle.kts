333fe0a1b46ac75575d00a5e16dcc99cdc11fc5e
Stephen Wanjala <stephenwanjala145@gmail.com>
2024-10-30 10:21:15 +0300

    Some Clean Ups

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,9 @@ val commonsCodecVersion: String by project
 val kmongoVersion: String by project
 
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
     id("io.ktor.plugin") version "3.0.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
 }
 
 group = "com.github.stephenwanjala"
--- src/main/kotlin/com/github/stephenwanjala/plugins/Security.kt
+++ src/main/kotlin/com/github/stephenwanjala/plugins/Security.kt
@@ -2,16 +2,12 @@ package com.github.stephenwanjala.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import com.auth0.jwt.interfaces.Payload
-import com.github.stephenwanjala.auth.data.database.UserService
 import com.github.stephenwanjala.auth.domain.repository.AuthRepository
 import com.github.stephenwanjala.auth.security.token.TokenConfig
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
-import org.jetbrains.exposed.sql.select
 
-data class UserPrincipal(val payload: Payload) : Principal
 fun Application.configureSecurity(config: TokenConfig,authRepository: AuthRepository) {
 
     // Please read the jwt property from the config file if you are using EngineMain
